a185574405e6b26286c9e265eb1ee240e78c4f1c
Gary McNeely <8042200+garymcneelyit@users.noreply.github.com>
2018-10-13 15:22:53 -0500

    Math.java
    class Math
    
    {  public static void main ( String[] args )
    
    {     // defining number for program
    	int num = 50 ;
    	int factor = 2 ;
    	int sum = 0 ;
         // calculations
    	sum = num + factor ;
    	System.out.println ( "add these together " + sum );
    	sum = num - factor ;
    	System.out.println ( "minus these values " + sum );
    	sum = num * factor ;
    	System.out.println ( "times these together" + sum );
    	sum = num / factor ;
    	System.out.println ( "divide these values" + sum );
    	
    }
    
    
    
    
    
    }

---
+++ Math.java
@@ -0,0 +1,25 @@
+class Math
+
+{  public static void main ( String[] args )
+
+{     // defining number for program
+	int num = 50 ;
+	int factor = 2 ;
+	int sum = 0 ;
+     // calculations
+	sum = num + factor ;
+	System.out.println ( "add these together " + sum );
+	sum = num - factor ;
+	System.out.println ( "minus these values " + sum );
+	sum = num * factor ;
+	System.out.println ( "times these together" + sum );
+	sum = num / factor ;
+	System.out.println ( "divide these values" + sum );
+	
+}
+
+
+
+
+
+}
