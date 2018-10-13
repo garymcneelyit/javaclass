58c41fc9803cea2f7636c1ecab37fb84d29786d1
Gary McNeely <8042200+garymcneelyit@users.noreply.github.com>
2018-10-13 15:21:10 -0500

    DataTypes.java
    class DataTypes
    {
    	public static void main ( String[] args ) 	
    {               char letter = 'M' ;
    		String title = "data types video 4 u" ;
    		int number = 365 ;
    		float decimal = 32.0f;
    	        boolean result = true;
    
    		System.out.println( "Initial is " + letter );
    		System.out.println( "Book is " + title );
    		System.out.println( "days are " + number );
    		System.out.println( "temperature is " + decimal );
    		System.out.println( "answer is " + result );
    
    }
    
    
    
    }

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+class DataTypes
+{
+	public static void main ( String[] args ) 	
+{               char letter = 'M' ;
+		String title = "data types video 4 u" ;
+		int number = 365 ;
+		float decimal = 32.0f;
+	        boolean result = true;
+
+		System.out.println( "Initial is " + letter );
+		System.out.println( "Book is " + title );
+		System.out.println( "days are " + number );
+		System.out.println( "temperature is " + decimal );
+		System.out.println( "answer is " + result );
+
+}
+
+
+
+}
